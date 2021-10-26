14dd5709aa5b7fdca404c7aa721f70f7e139b3d2
Carlos-madrigal83 <madrigalgallego.jc@gmail.com>
2021-10-26 12:06:29 +0200

    Casi terminado

---
+++ index.js
@@ -0,0 +1,64 @@
+//Tipos de datos
+
+//1.-
+//const notDefined
+
+//2.-
+//const firstNumber = 1
+//const secondNumber = 2
+/*
+    //3.-
+    let emptyString = ""
+
+    //4.-
+    const string = "asdf"
+
+    //5.-
+    let emptyArray = []
+
+    //6.-
+    const arrayOfNumbers = [1, 2, 3, 4, 5]
+
+    //7.-
+    const arrayOfStrings = ["", "Carlos", "Aroa", "Naiara", "Irene"]
+
+    //8.-
+    const emptyObject = {}
+
+    //9.-
+    let arrayOfObjects = {
+        marca: "Seat",
+        modelo: "León"
+    }
+
+    //10.-
+    const object = {
+        name: "Carlos",
+        age: 37,
+        hobbies: ["Futbol", "Padel", "Motos"]
+    }
+
+    //11.-
+    let boolean = true
+
+    //12.-
+    let oppositeBoolean = !boolean
+
+    //Funciones
+
+    //1.-
+function multiplyTwoNumbers(firstNumber, secondNumber) {
+    firstNumber * secondNumber;
+    console.log(multiplyTwoNumbers)
+}
+multiplyTwoNumbers()*/
+
+function multiplyTwoNumbers(a, b) {
+    let resultado = a * b
+    console.log(resultado)
+}
+multiplyTwoNumbers(2, 4)
+
+function stringToUpperCase(carlos) {
+
+}
